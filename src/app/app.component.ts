c5bfe10d34542c5441272c7d3ac4eaed3caa9b89
muhia8943 <nmmuhia@gmail.com>
2024-06-14 18:49:16 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { NotesDashboardComponent } from './notes-dashboard/notes-dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LandingComponent,LoginComponent,RegisterComponent,NotesDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
